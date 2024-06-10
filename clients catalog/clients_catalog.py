86d653ecf28cd468f5cb56a72e76ce56c97ba3f6
riccardof0235 <101888915+riccardof0235@users.noreply.github.com>
2024-06-10 12:36:44 +0200

    Add files via upload

---
+++ clients catalog/clients_catalog.py
@@ -0,0 +1,257 @@
+import cherrypy
+import json
+import os
+import sys
+import requests
+import time
+from etc.clients_class import *
+
+class Registration_deployer(object):
+    exposed=True
+    def __init__(self,filename):
+        self.filename=filename
+        self.catalog=ClientsCatalog(self.filename)
+        self.service=self.catalog.registerRequest()
+
+    def GET(self,*uri,**params):
+        if (len(uri))>0 and uri[0]=="reg":
+            return open('etc/reg.html')
+
+        elif (len(uri)>0 and uri[0]=="reg_results"):
+            check=self.catalog.check_registration(params['userID'],params['platformID'])
+            if check is not False:
+                html_error="etc/fail_reg_"+check+".html"
+                return open(html_error)
+                
+            if params['psw']!=params['psw-repeat']:
+                return open("etc/fail_reg_pass.html") 
+
+            else:
+                self.catalog.platforms.set_value(params['platformID'],"associated",True)
+                self.catalog.users.content["users"].append({
+                    "username":params['userID'],
+                    "password":params['psw'],
+                    "platforms_list":[params["platformID"]]
+                    })
+                try:
+                    profiles_catalog=self.catalog.retrieveService("profiles_catalog")
+                    r=requests.put(profiles_catalog['url']+"/insertProfile",json={"platform_ID":params['platformID']})
+                    r.raise_for_status()
+                       
+                except:
+                    self.catalog.users.removeUser(params['userID'])
+                    self.catalog.platforms.set_value(params['platformID'],"associated",False)
+                    return open("etc/error.html")
+                    
+                self.catalog.users.save()
+                self.catalog.platforms.save()
+
+                print("User '{}' correctly registered with platform '{}'\n".format(params['userID'],params['platformID']))
+                return open("etc/correct_reg.html")
+        
+        elif(len(uri))>0 and uri[0]=="platforms_list":
+            username=params['username']
+            try:
+                data=self.catalog.users.find_user(username).copy()
+                print(data)
+                #del data['password']
+                return json.dumps(data['platforms_list'])
+            except:
+                raise cherrypy.HTTPError(400,"Bad Request!")
+
+        elif(len(uri))>0 and uri[0]=="checkAssociation":
+            result=self.catalog.check_association(uri[1])
+            return json.dumps({"result":result})
+
+        elif(len(uri))>0 and uri[0]=="tokens":
+            result=self.catalog.platforms.content['tokens']
+            return json.dumps(result)
+
+        #avoiding sharing keys on github
+        elif(len(uri))>0 and uri[0]=="temp_tokens":
+            result=json.load(open('database/temp_token.json',"r"))
+            return json.dumps(result)
+
+        #get patients list associated with thingspeak channel
+        elif (len(uri))>0 and uri[0]=="associated_patients":
+            platform=self.catalog.platforms.find_platform(uri[1])
+            if platform is not False:
+                try:
+                    log=platform['specs'][uri[2]]
+                    output=[]
+                    for i in log:
+                        output.append(i['patient'])
+                    return json.dumps(output)
+                except:
+                    raise cherrypy.HTTPError(400, "Bad request!")
+            else:
+                raise cherrypy.HTTPError("404 Platform not found!")
+
+
+        elif(len(uri))==3 and uri[0]=="info":
+            platform=self.catalog.platforms.find_platform(uri[1])
+            if platform is not False:
+                try:
+                    result=json.dumps(platform['specs'][uri[2]])
+                    return result
+                except:
+                    raise cherrypy.HTTPError(400, "Bad request!")
+            else:
+                raise cherrypy.HTTPError(404, "Platform not found!")
+                
+                
+        else:
+            raise cherrypy.HTTPError("501 No operation!")
+            
+    def POST(self,*uri):
+        if(len(uri))>0 and uri[0]=="login":
+            body=cherrypy.request.body.read()
+            json_body=json.loads(body.decode('utf-8'))
+            username=json_body['username']
+            password=json_body['password']
+            try:
+                data=self.catalog.users.login(username,password).copy()
+                try:
+                    chatID=int(json_body['chat_ID'])
+                    user=self.catalog.users.find_user(username)
+                    for platform in user['platforms_list']:
+                        self.catalog.platforms.add_chatID(platform,chatID)
+                    self.catalog.platforms.save()
+                except:
+                    pass
+                print(data)
+                del data['password']
+                return json.dumps(data)
+            except:
+                raise cherrypy.HTTPError("401 Login failed")
+    def PUT(self,*uri):
+        command=str(uri[0])
+        body=cherrypy.request.body.read()
+        json_body=json.loads(body.decode('utf-8'))
+        print(json_body)
+        if command=="newPlatform":
+            user=self.catalog.users.find_user(json_body['username'])
+            if self.catalog.platforms.find_platform(json_body['platformID']) is not False:
+                if not self.catalog.check_association(json_body['platformID']) and user is not False:
+                    profiles_catalog=self.catalog.retrieveService("profiles_catalog")
+                    r=requests.put(profiles_catalog['url']+"/insertProfile",json={"platform_ID":json_body['platformID']})
+                    if r.status_code==200:
+                        user['platforms_list'].append(json_body['platformID'])
+                        self.catalog.platforms.set_value(json_body['platformID'],"associated",True)
+                        self.catalog.users.save()
+                        self.catalog.platforms.save()
+                        print("Platform {} correctly associated.".format(json_body['platformID']))
+                            
+                    else:
+                        return r
+                else:
+                    raise cherrypy.HTTPError("409 Already exists!")
+            else:
+                raise cherrypy.HTTPError("404 Platform not available!")
+                
+
+        elif command=="newpatient":
+            output=self.catalog.platforms.associate_patient_thingspeak(json_body['platformID'],json_body['patientID'])
+            if output is not False:
+                self.catalog.platforms.save()
+                if output=="Available thingspeak channel found! Registered...":
+                    grafana_catalog=self.catalog.retrieveService('grafana_catalog')
+                    log=requests.post(grafana_catalog['url']+"/"+json_body['platformID']+"/"+json_body['patientID']+"/createDashboard")
+                print("{} - {}".format(json_body['platformID'],json_body['patientID']))
+                print(output)
+                return json.dumps({'msg':output})
+            else:
+                raise cherrypy.HTTPError("404 Platform not found!")
+
+        else:
+            raise cherrypy.HTTPError("501 No operation!")
+
+    def DELETE(self,*uri):
+        command=str(uri[0])
+        if command=='removePlatform':
+            try:
+                username=uri[1]
+                platform_ID=uri[2]
+            except:
+                raise cherrypy.HTTPError("400 Missing parameters!")
+            outputFlag=self.catalog.users.removePlatform(username,platform_ID)
+            if outputFlag:
+                self.catalog.platforms.removePlatform(platform_ID)
+                output="Platform '{}' removed".format(platform_ID)
+                print(output)
+                self.catalog.users.save()
+                self.catalog.platforms.save()
+            else:
+                output="Platform '{}' not found ".format(platform_ID)
+                print(output)
+                raise cherrypy.HTTPError("404 Resource not found")
+
+        elif command=='removepatient':
+            try:
+                username=uri[1]
+                platform_ID=uri[2]
+                patient_ID=uri[3]
+            except:
+                raise cherrypy.HTTPError("400 Missing parameters!")
+            if self.catalog.users.find_user(username) and platform_ID in self.catalog.users.find_user(username)["platforms_list"]:
+                outputFlag=self.catalog.platforms.removepatient(platform_ID,patient_ID)
+                if outputFlag:
+                    platform=self.catalog.platforms.find_platform(platform_ID)
+                    org_key=platform['specs']['grafana']["org_key"]
+                    print(org_key)
+                    grafana_catalog=self.catalog.retrieveService('grafana_catalog')
+                    print(grafana_catalog)
+                    log=requests.delete(grafana_catalog['url']+"/"+platform_ID+"/"+patient_ID+"/deleteDashboard/"+org_key)
+                    output="Platform '{}' - patient '{}' removed".format(platform_ID,patient_ID)
+                    print(output)
+                    self.catalog.platforms.save()
+            else:
+                raise cherrypy.HTTPError("404 Resource not found")
+
+        elif command=='removeUser':
+            username=uri[1]
+            user=self.catalog.users.find_user(username)
+            platforms_list=user['platforms_list']
+
+            if user is not False:
+                outputFlag=self.catalog.users.removeUser(user)
+                if outputFlag:
+                    for platform in platforms_list:
+                        self.catalog.platforms.removePlatform(platform)
+                    output="User '{}' removed".format(username)
+                    print(output)
+                    self.catalog.platforms.save()
+                    self.catalog.users.save()
+                else:
+                    raise cherrypy.HTTPError("500 Operation can't be performed.")
+
+            else:
+                raise cherrypy.HTTPError("404 User not found")
+
+        elif command=='removeChatID':
+            platform_ID=uri[1]
+            self.catalog.platforms.remove_chatID(platform_ID,int(uri[2]))
+            self.catalog.platforms.save()
+        
+        else:
+            raise cherrypy.HTTPError("501 No operation!")
+
+    
+if __name__ == '__main__':
+    clients_db=sys.argv[1]
+    clientsCatalog=Registration_deployer(clients_db)
+    if clientsCatalog.service is not False:
+        conf = {
+            '/': {
+                'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+                'tools.staticdir.root': os.path.abspath(os.getcwd()),
+                'tools.sessions.on': True
+            }
+        }
+
+        cherrypy.tree.mount(clientsCatalog, clientsCatalog.service, conf)
+        cherrypy.config.update({'server.socket_host':clientsCatalog.catalog.serviceIP})
+        cherrypy.config.update({'server.socket_port':clientsCatalog.catalog.servicePort})
+        cherrypy.engine.start()
+
+        cherrypy.engine.block()
---
+++ clients catalog/etc/clients_class.py
@@ -0,0 +1,174 @@
+import json
+from etc.generic_service import Generic_Service
+
+class PlatformsCatalog():
+    def __init__(self,db_filename):
+        self.db_filename=db_filename
+        self.content=json.load(open(self.db_filename,"r"))
+        
+    def set_value(self,platform_ID,field,value):
+        platform=self.find_platform(platform_ID)
+        platform[field]=value
+        
+
+    def add_chatID(self,platform_ID,chatID):
+        platform=self.find_platform(platform_ID)
+        try:
+            chatID_list=platform['specs']['chatIDs']
+            if chatID not in chatID_list:
+                chatID_list.append(int(chatID))
+        except:
+            platform['specs']['chatIDs']=[]
+            platform['specs']['chatIDs'].append(int(chatID))
+
+        
+    def find_platform(self,platform_ID):
+        notFound=1
+        for platform in self.content['platforms']:
+            if platform['platform_ID']==platform_ID:
+                notFound=0
+                break
+        if notFound==1:
+            return False
+        else:
+            return platform
+
+    def associate_patient_thingspeak(self,platform_ID,patient_ID):
+        platform=self.find_platform(platform_ID)
+        notFound=1
+        if platform is not False:
+            for spec in platform['specs']['thingspeak']:
+                if spec['patient'] is None:
+                    spec['patient']=patient_ID
+                    output="Available thingspeak channel found! Registered..."
+                    notFound=0
+                    break
+
+            if notFound==1:
+                output="No Available thingspeak channel!" 
+            return output
+        else:
+            return False
+
+    def removePlatform(self,platform_ID):
+        self.set_value(platform_ID,"associated",False)
+        self.remove_all_patients(platform_ID)
+
+    def removepatient(self,platform_ID,patient_ID):
+        notFound=1
+        platform=self.find_platform(platform_ID)
+        if platform is not False:
+            for spec in platform['specs']['thingspeak']:
+                if spec['patient'] ==patient_ID:
+                    spec['patient']=None
+                    notFound=0
+                    return True
+            if notFound==1:
+                return False
+        else:
+            return False
+
+    def remove_all_patients(self,platform_ID):
+        platform=self.find_platform(platform_ID)
+        platform['specs']['chatIDs'].clear()
+        for spec in platform['specs']['thingspeak']:
+            self.removepatient(platform_ID,spec['patient'])
+
+    def remove_chatID(self,platform_ID,chatID):
+        platform=self.find_platform(platform_ID)
+        chatID_list=platform['specs']['chatIDs']
+        chatID_list.remove(chatID)
+
+    def save(self):
+        with open(self.db_filename,'w') as file:
+            json.dump(self.content,file, indent=4)
+        
+class UsersCatalog():
+    def __init__(self, db_filename):
+        self.db_filename=db_filename
+        self.content=json.load(open(self.db_filename,"r"))
+        self.createDict()
+        
+    def createDict(self):
+        d = self.content['users']
+        self.userpassdict = dict((i["username"],["password"]) for i in d)
+        
+    def find_user(self,username):
+        notFound=1
+        for user in self.content['users']:
+            if user.get('username').lower()==username.lower():
+                notFound=0
+                break
+        if notFound==1:
+            return False
+        else:
+            return user
+        
+    def login(self,username,password):
+        user=self.find_user(username)
+        if user is not False and user['password']==password:
+            return user
+        else:
+            return False  
+        
+    def removePlatform(self,username,platform_ID):
+        user=self.find_user(username)
+        if user is not False:
+            try:
+                user['platforms_list'].remove(platform_ID)
+                return True
+            except Exception as e:
+                return False
+        else:
+            return False
+    
+    def removeUser(self,username):
+        try:
+            self.content['users'].remove(user)
+            return True
+        except Exception as e:
+            return False
+
+    def save(self):
+        self.createDict()
+        with open(self.db_filename,'w') as file:
+            json.dump(self.content,file, indent=4)
+    
+
+class ClientsCatalog(Generic_Service):
+    def __init__(self, conf_filename,users_filename="database/users.json",platforms_filename="database/platforms.json"):
+        Generic_Service.__init__(self,conf_filename)
+        self.users_filename=users_filename
+        self.platforms_filename=platforms_filename
+        self.users=UsersCatalog(self.users_filename)
+        self.platforms=PlatformsCatalog(self.platforms_filename)
+        
+    def check_registration(self,username,platform_ID):
+        if self.users.find_user(username) is not False:
+            return "user"
+        platform=self.platforms.find_platform(platform_ID)
+        if platform is not False:
+            if platform['associated']:
+                return "associated"
+            else:
+                return False
+        else:
+            return "platform"
+        
+    def check_association(self,platform_ID):
+        platform=self.platforms.find_platform(platform_ID)
+        return platform['associated']
+            
+        
+            
+            
+        
+
+
+
+
+
+
+
+
+
